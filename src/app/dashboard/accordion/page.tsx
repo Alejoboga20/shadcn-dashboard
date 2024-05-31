13801e01d7afecf937e0c651181fcab694d46953
Alejoboga20 <alejoboga19@gmail.com>
2024-05-31 11:16:56 -0300

    use accordion component

--- src/app/dashboard/accordion/page.tsx
+++ src/app/dashboard/accordion/page.tsx
@@ -1,5 +1,41 @@
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+
+const items = [
+  {
+    id: "item-1",
+    question: "Is it accessible?",
+    answer: "Yes. It adheres to the WAI-ARIA design pattern.",
+  },
+  {
+    id: "item-2",
+    question: "Can I use it in a commercial project?",
+    answer: "Yes. It's open-source and free to use.",
+  },
+  {
+    id: "item-3",
+    question: "Does it require any build tools?",
+    answer: "No. It's written in plain JavaScript.",
+  },
+];
+
 const Page = () => {
-	return <div>Accordion Page</div>;
+  return (
+    <div>
+      <Accordion type="multiple" className="w-full">
+        {items.map((item) => (
+          <AccordionItem value={item.id} key={item.id}>
+            <AccordionTrigger>{item.question}</AccordionTrigger>
+            <AccordionContent>{item.answer}</AccordionContent>
+          </AccordionItem>
+        ))}
+      </Accordion>
+    </div>
+  );
 };
 
 export default Page;
