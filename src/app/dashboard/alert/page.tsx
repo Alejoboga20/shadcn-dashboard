5ab39222c653412079bb09eb011246c37d668f72
Alejoboga20 <alejoboga19@gmail.com>
2024-05-31 11:42:59 -0300

    use alert component

--- src/app/dashboard/alert/page.tsx
+++ src/app/dashboard/alert/page.tsx
@@ -1,5 +1,34 @@
+import { Alert, AlertTitle, AlertDescription } from "@/components/ui/alert";
+import { RocketIcon } from "@radix-ui/react-icons";
+
 const Page = () => {
-	return <div>Accordion Page</div>;
+  return (
+    <div className="grid gap-3">
+      <Alert>
+        <RocketIcon className="h-4 w-4" />
+        <AlertTitle>Heads up!</AlertTitle>
+        <AlertDescription>
+          You can add components and dependencies to your app using the cli.
+        </AlertDescription>
+      </Alert>
+
+      <Alert variant="success">
+        <RocketIcon className="h-4 w-4" />
+        <AlertTitle>Heads up!</AlertTitle>
+        <AlertDescription>
+          You can add components and dependencies to your app using the cli.
+        </AlertDescription>
+      </Alert>
+
+      <Alert variant="destructive">
+        <RocketIcon className="h-4 w-4" />
+        <AlertTitle>Heads up!</AlertTitle>
+        <AlertDescription>
+          You can add components and dependencies to your app using the cli.
+        </AlertDescription>
+      </Alert>
+    </div>
+  );
 };
 
 export default Page;
